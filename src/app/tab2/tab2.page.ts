c277a43c49cccbcb7432377c8db56bb55a196eec
Nathan Vu <nathan.vu5@gmail.com>
2019-11-21 16:35:49 -0800

    updated camera feature v2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -10,9 +10,26 @@ import { PhotoService } from '../services/photo.service';
 export class Tab2Page {
 
   photos: any;
+  currentImage: string;
 
   // tslint:disable-next-line: no-shadowed-variable
   constructor( private camera: Camera, public photoService: PhotoService) { }
 
 
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.currentImage = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+      console.log('Camera issue:' + err);
+    });
+  }
 }
+
